d45dadd09dd6c7a5f9398de225d67bc47fc8c2cf
kbdonnally <kbdonnally@gmail.com>
2021-09-18 15:09:27 -0400

    Create contact page template and social media partial

---
+++ template-parts/social-media.php
@@ -0,0 +1,33 @@
+<section class="social-media-wrapper">
+	<h2 class="sm__title">Social Media</h2>
+
+	<section class="sm__content">
+		<div class="sm__flex-wrapper">
+			<ul class="sm__row">
+				<li class="sm__svg"><img alt="Twitter logo" src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/twitter-black.svg' ?>"></li>
+				<li class="sm__type">Twitter:</li>
+				<li class="sm__user"><a href="https://twitter.com/dsarichmond">@dsarichmond</a></li>
+			</ul>
+			<ul class="sm__row">
+				<li class="sm__svg"><img alt="Instagram logo" src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/instagram-black.svg' ?>"></li>
+				<li class="sm__type">Instagram:</li>
+				<li class="sm__user"><a href="https://www.instagram.com/dsarichmond/?hl=en">dsarichmond</a></li>
+			</ul>
+			<ul class="sm__row">
+				<li class="sm__svg"><img alt="Facebook logo" src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/facebook-black.svg' ?>"></li>
+				<li class="sm__type">Facebook:</li>
+				<li class="sm__user"><a href="https://www.facebook.com/DSARichmond/">DSA Richmond</a></li>
+			</ul>
+			<ul class="sm__row">
+				<li class="sm__svg"><img alt="Email logo" src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/envelope-outline-black.svg' ?>"></li>
+				<li class="sm__type">Email:</li>
+				<li class="sm__user"><a href="mailto:admin@dsarichmond.org">admin@dsarichmond.org</a></li>
+			</ul>
+			<ul class="sm__row">
+				<li class="sm__svg"><img alt="Patreon logo" src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/patreon-black.svg' ?>"></li>
+				<li class="sm__type">Patreon:</li>
+				<li class="sm__user"><a href="https://www.patreon.com/dsarichmond">patreon.com/dsarichmond</a></li>
+			</ul>
+		</div>
+	</section>
+</section>
