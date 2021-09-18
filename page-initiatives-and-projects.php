a2f14e5f64c0a3e6113acff47cfb24c61adb6c61
kbdonnally <kbdonnally@gmail.com>
2021-09-18 17:03:24 -0400

    Make real links

--- page-initiatives-and-projects.php
+++ page-initiatives-and-projects.php
@@ -12,7 +12,8 @@
 					while ($p->have_posts()) {
 						$p->the_post(); ?>
 						<li class="projects-init__list-item">
-							<h2 class="projects-item__title"><a href="#"><?php the_title(); ?></a></h2>
+							<h2 class="projects-item__title">
+								<a href="<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a></h2>
 							<h3 class="projects-item__subtitle"><?php echo get_secondary_title(); ?></h3>
 							<div class="projects-item__summary"><?php the_excerpt() ?></div>
 							<h3 class="projects-item__cta"><?php the_shortlink("Learn more"); ?></h3>
@@ -24,4 +25,5 @@
 			</ul> <!-- end list -->
 		</main> <!-- end content -->
 </main>
+
 <?php get_footer() ?>
