d71f83df06c04ca415f1a26b7f75c96b83ad059f
kbdonnally <kbdonnally@gmail.com>
2021-09-18 15:25:37 -0400

    Fix typos in header

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,200;1,300;1,700;1,900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
-  <link rel="stylesheet" href="<?php echo get_site_url() . '/wp-content/themes/dsarichmond/style.css'; ?> "
+  <link rel="stylesheet" href="<?php echo get_site_url() . '/wp-content/themes/dsarichmond/style.css'; ?> ">
 </head>
 <body>
 <?php wp_body_open(); ?>
