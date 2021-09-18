0313b7b8019c7476214828e5022e6854f7f60538
kbdonnally <kbdonnally@gmail.com>
2021-09-18 15:26:45 -0400

    Spacing issue

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 	      		<a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/logo_dsa-richmond.svg'; ?>" alt="Richmond DSA logos"></a>
 	    	</div>
 	    <div class="nav-hamburger-icon">
-	    	<img src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/hamburger-icon.svg'?>"" alt="hamburger icon">
+	    	<img src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/hamburger-icon.svg'?>" alt="hamburger icon">
 	    </div>
 	    <div class="nav-close-icon nav-content--hide"><img src="<?php echo get_site_url() . '/wp-content/uploads/2021/09/x.svg' ?>" alt="hamburger icon"></div>
 	    <ul class="nav-content__link-list">
