7de7bb2d670c0ed36e376651ef77d65240b5b76a
kbdonnally <kbdonnally@gmail.com>
2021-09-23 15:40:53 -0400

    Add excerpts to resources

--- functions.php
+++ functions.php
@@ -185,4 +185,5 @@ wp_enqueue_script('main', get_template_directory() . '/assets/js/main.js');
 // Adding excerpt for page
 add_post_type_support( 'page', 'excerpt' );
 add_post_type_support( 'initiatives', 'excerpt' );
+add_post_type_support( 'resources', 'excerpt' );
 add_post_type_support('page', 'custom-fields');
