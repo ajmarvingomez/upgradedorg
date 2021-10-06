0834dc1dc4e9c21933bf951cd265bf4f576d1579
kbdonnally <kbdonnally@gmail.com>
2021-10-06 15:16:46 -0400

    Create embedded newsletter form as template part

--- template-parts/newsletter.php
+++ template-parts/newsletter.php
@@ -1,41 +1,37 @@
 <section class="newsletter-wrapper">
 	<div class="newsletter-content">
-		<h1 class="newsletter__title">Sign-Up for Our Newsletter “The Richmond Rose”</h1>
-		<p>Every week, we share upcoming community events and DSA initiatives or news related to socialism. Sign up today to get in touch or get engaged with Richmond DSA in our community!</p>
+		<h1 class="newsletter__title">Sign Up for Our Newsletter “The Richmond Rose”</h1>
 
-		<!-- Begin Mailchimp Signup Form -->
-		<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-		<style type="text/css">
-			#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-			/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-		</style>
-		<div id="mc_embed_signup">
-		<form action="https://dsarichmond.us15.list-manage.com/subscribe/post?u=85518fb08bcc284befc646aa3&amp;id=19e4be95d1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-		    <div id="mc_embed_signup_scroll">
-			<h2>Subscribe</h2>
-		<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-		<div class="mc-field-group">
-			<label for="mce-NAME">Your Name  <span class="asterisk">*</span>
-		</label>
-			<input type="text" value="" name="NAME" class="required" id="mce-NAME">
+		<p class="newsletter__text">Every week, we share upcoming community events and DSA initiatives or news related to socialism. Sign up today to get in touch or get engaged with Richmond DSA in our community!</p>
+
+		<div class="newsletter__form">
+			<!-- Begin Mailchimp Signup Form -->
+			<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+			<div id="mc_embed_signup">
+			<form action="https://dsarichmond.us15.list-manage.com/subscribe/post?u=85518fb08bcc284befc646aa3&amp;id=19e4be95d1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			    <div id="mc_embed_signup_scroll">
+			<!--div class="indicates-required"><span class="asterisk">*</span> indicates required</div-->
+			<div class="mc-field-group">
+				<label for="mce-NAME">Your Name  <span class="asterisk">*</span>
+			</label>
+				<input type="text" value="" name="NAME" class="required" id="mce-NAME">
+			</div>
+			<div class="mc-field-group">
+				<label for="mce-EMAIL">Your Email Address  <span class="asterisk">*</span>
+			</label>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+				<div id="mce-responses" class="clear" style="width: 0 !important;">
+					<div class="response" id="mce-error-response" style="display:none"></div>
+					<div class="response" id="mce-success-response" style="display:none"></div>
+				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_85518fb08bcc284befc646aa3_19e4be95d1" tabindex="-1" value=""></div>
+			    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			    </div>
+			</form>
+			</div>
+			<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[3]='NAME';ftypes[3]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+			<!--End mc_embed_signup-->
 		</div>
-		<div class="mc-field-group">
-			<label for="mce-EMAIL">Your Email Address  <span class="asterisk">*</span>
-		</label>
-			<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-		</div>
-			<div id="mce-responses" class="clear">
-				<div class="response" id="mce-error-response" style="display:none"></div>
-				<div class="response" id="mce-success-response" style="display:none"></div>
-			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-		    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_85518fb08bcc284befc646aa3_19e4be95d1" tabindex="-1" value=""></div>
-		    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-		    </div>
-		</form>
-		</div>
-		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[3]='NAME';ftypes[3]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-		<!--End mc_embed_signup-->
-	
 	</div>
 </section>
