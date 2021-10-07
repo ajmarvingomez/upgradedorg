da00168970d336ad89894810ce221fc2eec12989
kbdonnally <kbdonnally@gmail.com>
2021-10-07 13:57:09 -0400

    Add generic page.php

--- page.php
+++ page.php
@@ -12,27 +12,22 @@
  * @package dsarichmond
  */
 
-get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<?php get_header(); ?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+<main class="page-wrapper">
+		<div class="page-title-wrapper"><h1 class="page__title"><?php the_title(); ?></h1></div>
+		
+		<main class="page__content">
+			<section class="page-content__text">
+				<?php the_content(); ?>
+			</section>
+			
+			<?php get_template_part('template-parts/social-media'); ?>
+		</main>
+</main>
 
-			get_template_part( 'template-parts/content', 'page' );
+<?php get_template_part('template-parts/newsletter'); ?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_footer();
-?>
+<?php get_footer(); ?>
