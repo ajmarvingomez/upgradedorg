519b3a40947eb22088b10ea975499933308f6856
ajmarvingomez <ajmarvingomez@gmail.com>
2024-08-19 13:44:30 -0400

    fix: add null checks hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,46 +3,48 @@
 let burgerIcon = document.querySelector('.nav-hamburger-icon');
 let xIcon = document.querySelector('.nav-close-icon');
 let menuList = document.querySelector('.nav-content__link-list');
-window.onload = () => {
-	let width = window.innerWidth;
+if( null !== burgerIcon && null !== xIcon && null !== menuList) {
+	window.addEventListener('DOMContentLoaded',() => {
+		let width = window.innerWidth;
 
-	if (width < 1000) {
-		burgerIcon.onclick = () => {
+		if (width < 1000) {
+			burgerIcon.onclick = () => {
+				burgerIcon.style.display = 'none';
+				xIcon.style.display = 'flex';
+				menuList.style.display = 'flex';
+			};
+			xIcon.onclick = () => {
+				xIcon.style.display = 'none';
+				menuList.style.display = 'none';
+				burgerIcon.style.display = 'flex';
+			};
+		} else {
 			burgerIcon.style.display = 'none';
-			xIcon.style.display = 'flex';
-			menuList.style.display = 'flex';
-		};
-		xIcon.onclick = () => {
 			xIcon.style.display = 'none';
-			menuList.style.display = 'none';
+		}
+	})
+
+	window.addEventListener('resize', () => {
+		let width = window.innerWidth;
+		menuList.style.display = 'none';
+
+		xIcon.style.display = 'none;';
+		if (width < 1000) {
 			burgerIcon.style.display = 'flex';
-		};
-	} else {
-		burgerIcon.style.display = 'none';
-		xIcon.style.display = 'none';
-	}
-};
-
-window.addEventListener('resize', () => {
-	let width = window.innerWidth;
-	menuList.style.display = 'none';
-
-	xIcon.style.display = 'none;';
-	if (width < 1000) {
-		burgerIcon.style.display = 'flex';
-		burgerIcon.onclick = () => {
+			burgerIcon.onclick = () => {
+				menuList.style.display = 'flex';
+				xIcon.style.display = 'flex';
+				burgerIcon.style.display = 'none';
+			};
+			xIcon.onclick = () => {
+				burgerIcon.style.display = 'flex';
+				xIcon.style.display = 'none';
+				menuList.style.display = 'none';
+			};
+		} else {
 			menuList.style.display = 'flex';
-			xIcon.style.display = 'flex';
 			burgerIcon.style.display = 'none';
-		};
-		xIcon.onclick = () => {
-			burgerIcon.style.display = 'flex';
 			xIcon.style.display = 'none';
-			menuList.style.display = 'none';
-		};
-	} else {
-		menuList.style.display = 'flex';
-		burgerIcon.style.display = 'none';
-		xIcon.style.display = 'none';
-	}
-});
+		}
+	});
+}
